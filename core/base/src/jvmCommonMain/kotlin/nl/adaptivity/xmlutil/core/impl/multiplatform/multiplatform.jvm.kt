1cf50c8c50c3b8c0eac07b19b2f0ace04500bfbd
Paul de Vrieze <paul.devrieze@gmail.com>
2026-03-09 18:35:05 +0000

    Add extended support for assertions, including a conditionally executed block that only runs if assertions run.

--- core/base/src/jvmCommonMain/kotlin/nl/adaptivity/xmlutil/core/impl/multiplatform/multiplatform.jvm.kt
+++ core/base/src/jvmCommonMain/kotlin/nl/adaptivity/xmlutil/core/impl/multiplatform/multiplatform.jvm.kt
@@ -1,27 +1,32 @@
 /*
- * Copyright (c) 2024.
+ * Copyright (c) 2024-2026.
  *
  * This file is part of xmlutil.
  *
- * This file is licenced to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You should have received a copy of the license with the source distribution.
- * Alternatively, you may obtain a copy of the License at
+ * This file is licenced to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
+ * with the License.  You should have  received a copy of the license
+ * with the source distribution. Alternatively, you may obtain a copy
+ * of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 package nl.adaptivity.xmlutil.core.impl.multiplatform
 
 import nl.adaptivity.xmlutil.XmlUtilInternal
 import kotlin.io.use as ktUse
+
+
+@XmlUtilInternal
+public actual val Any.assertionsEnabled: Boolean
+    get() = javaClass.desiredAssertionStatus()
 
 public actual fun assert(value: Boolean, lazyMessage: () -> String) {
     kotlin.assert(value, lazyMessage)
